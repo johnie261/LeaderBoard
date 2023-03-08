1b2b63389af24ea9a460931ea831f1424b19b6c3
johnie261 <njorogejohn261@gmail.com>
2023-03-08 16:44:44 +0300

    Add modules files

---
+++ src/modules/display.js
@@ -0,0 +1,11 @@
+const scoresContainer = document.querySelector('.scores-list');
+export default async (Games) => {
+    scoresContainer.innerHTML = '';
+    Games.sort((a,b) => b.score - a.score)
+    for (let i = 0; i < Games.length; i += 1) {
+        scoresContainer.innerHTML += `
+        <div class='scoreBoard'>
+            <p class='Title'>'${Games[i].user}': ${Games[i].score}</p>
+        </div>`;
+    };     
+};
