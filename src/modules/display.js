10a50a3cf0806a8be2d8dd9e661340f3f59d010a
johnie261 <njorogejohn261@gmail.com>
2023-03-09 17:34:07 +0300

    Update display.js file

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,11 +1,11 @@
-const scoresContainer = document.querySelector('.scores-list');
 export default async (Games) => {
+  const scoresContainer = document.querySelector('.scores-list');
   scoresContainer.innerHTML = '';
   Games.sort((a, b) => b.score - a.score);
   for (let i = 0; i < Games.length; i += 1) {
     scoresContainer.innerHTML += `
         <div class='scoreBoard'>
-            <p class='Title'>${Games[i].user}: ${Games[i].score}</p>
+            <p class='Title'>${i + 1}. ${Games[i].user}: ${Games[i].score}</p>
         </div>`;
   }
 };
